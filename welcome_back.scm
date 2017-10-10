0c7c65bc9f280dc29b1fa75a494cc9cb8100ee94
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-10-10 16:10:40 +0530

    Implement intersect-all with call-with-current-continuation

--- welcome_back.scm
+++ welcome_back.scm
@@ -50,3 +50,14 @@
                            ((member? (car s1) s2) (cons (car s1) (intersect1 (cdr s1))))
                            (else (intersect1 (cdr s1)))))))
     (intersect1 s1)))
+
+(define (intersect-all lset)
+  (call-with-current-continuation
+    (lambda (hop)
+      (letrec ((A (lambda (lset)
+                    (cond ((null? (car lset)) (hop '()))
+                          ((null? (cdr lset)) (car lset))
+                          (else (intersect (car lset)
+                                           (A (cdr lset))))))))
+        (cond ((null? lset) '())
+              (else (A lset)))))))
