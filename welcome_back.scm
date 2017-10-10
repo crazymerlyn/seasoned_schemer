ac48132556112bf423d54babe3936fca881ae0ff
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-10-10 15:33:20 +0530

    Implement union

--- welcome_back.scm
+++ welcome_back.scm
@@ -32,3 +32,10 @@
                          (else (or (eq? a (car lat))
                                    (yes? (cdr lat))))))))
     (yes? lat)))
+
+(define (union s1 s2)
+  (letrec ((union1 (lambda (s1)
+                     (cond ((null? s1) s2)
+                           ((member? (car s1) s2) (union1 (cdr s1)))
+                           (else (cons (car s1) (union1 (cdr s1))))))))
+    (union1 s1)))
